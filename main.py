7bea56b34467e38d8c36955c24721f7effd8afab
Robert <71367974+Pyroarti@users.noreply.github.com>
2022-12-21 15:50:47 +0100

    Added some test functions

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ Göra en API ifall man ska enkelt göra nya recept? (kanske man gör det via en 
 Alla datatyper man kan läsa och skriva
 
 Null:
-Boolean:
+Boolean:dsad
 SByte:
 Byte:
 Int16:
@@ -43,6 +43,10 @@ DiagnosticInfo:
 """
 # Kräver 2 bibliotek opcua-client och opcua
 from opcua import Client, ua
+import opcua.common
+from opcua.common import events
+import json
+from datetime import datetime
 
 
 def read_input_value(node_id): # Funktion för att läsa in en variabel och convertera den till en string
@@ -67,23 +71,62 @@ def write_value_bool(node_id, value):
     print("Value of : " + str(client_node) + ' : ' + str(client_node_value))
 
 
+def read_value_extension_object(node_id):
+    client_node = client.get_node(node_id)  
+    client_node_value = client_node.get_value() 
+    print(type(client_node_value))
+    #print(client_node_value)
+    print("Value of : " + str(client_node) + ' : ' + str(client_node_value))
+    
+
+
 if __name__ == "__main__":
 
 
-    client = Client("opc.tcp://192.168.0.1:4840")  # Adressen till servern
+    client = Client("opc.tcp://192.168.187.10:4840")  # Adressen till servern
     try:
         client.connect()
 
         root = client.get_root_node()
         print("Objektets root nod är: ", root) # Printar ut nod id
 
+        objects = client.get_objects_node()
+        print("Objects node is: ", objects)
+
+        print("Children of root are: ", root.get_children())
+
+        #obj2 = root.get_child(["0:Objects", "3:PLC_1"])
+        #print(obj2)
+
+
         
-        read_input_value("") # Sätt in nod id till vad du vill läsa in (i clienten högerklicka på variabeln och kopiera den)
+        #read_input_value('ns=3;s="StepData"."RecipeSteps"[10]."Active"') # Sätt in nod id till vad du vill läsa in (i clienten högerklicka på variabeln och kopiera den)
 
-        write_value_int("",) # Sätt in nod id till en int var och sen den nya värdet
+        #write_value_int("",) # Sätt in nod id till en int var och sen den nya värdet
 
-        write_value_bool("",) # Sätt in nod id till en bool var och sen den nya värdet
+        #write_value_bool("",) # Sätt in nod id till en bool var och sen den nya värdet
 
+        read_value_extension_object('ns=3;s="StepData"."RecipeSteps"[0]')
+        
 
     finally: # Bara ett try/finally block för att testa, byt sen mot whatever men bara för att säkerhetsställa att vi alltid lyckas disconnecta
         client.disconnect()
+
+
+
+
+# Kanske kan funka?
+# Now getting a variable node using its browse path
+        #obj = root.get_child(["0:Objects", "3:PLC_1,3:DataBlocksGlobal"])
+        #obj2 = root.get_child(["0:Root,0:Objects,3:PLC_1,3:DataBlocksGlobal,3:StepData,3:RecipeSteps,3:0"])
+
+#        print("MyObject is: ", obj)
+# get a specific node knowing its node id
+        #var = client.get_node(ua.NodeId(1002, 2))
+        #var = client.get_node("ns=3;i=2002")
+        #print(var)
+        #var.get_data_value() # get value of node as a DataValue object
+        #var.get_value() # get value of node as a python builtin
+        #var.set_value(ua.Variant([23], ua.VariantType.Int64)) #set node value using explicit data type
+        #var.set_value(3.9) # set node value using implicit data type
+        #0:Root,0:Objects,3:PLC_1,3:DataBlocksGlobal,3:StepData,3:RecipeSteps,3:0
